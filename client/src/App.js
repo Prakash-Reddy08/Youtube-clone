1ea5ecdaba72f78eda92b6ba3c37b8a29277cb2d
Prakash-Reddy08 <mjprakashreddy1@gmail.com>
2022-11-27 20:39:27 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components'
+import Menu from './components/Menu';
+import Navbar from './components/Navbar';
+import { darkTheme, lightTheme } from './utils/Theme';
+
+function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container>
+        <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+        <Main>
+          <Navbar />
+          <Wrapper>
+
+          </Wrapper>
+        </Main>
+      </Container>
+    </ThemeProvider>
+  )
+}
+const Container = styled.div`
+  display: flex;
+`
+const Main = styled.div`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+`
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`
+export default App;
